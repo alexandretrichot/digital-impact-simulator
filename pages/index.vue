89999e0d9d108604e5d4114b4d8812459d47e3f5
alexandretrichot <32410050+alexandretrichot@users.noreply.github.com>
2020-01-08 15:03:23 +0100

    Cleaning project

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,33 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        carbon_simulator
-      </h1>
-      <h2 class="subtitle">
-        A simulator for calculate your carbon footprint
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script>
-import Logo from '~/components/Logo.vue'
+import CarbonSimulator from "~/components/CarbonSimulator";
 
 export default {
   components: {
-    Logo
+    CarbonSimulator
   }
-}
+};
 </script>
-
+<template>
+  <div class="container">
+    <h1>Simulateur d'impact carbone numérique</h1>
+    <CarbonSimulator />
+  </div>
+</template>
 <style>
+* {
+  font-family: sans-serif;
+  box-sizing: border-box;
+}
+
+body {
+  background: black;
+  color: white;
+  margin: 0;
+}
+
 .container {
+  padding: 12px;
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  max-width: 600px;
 }
 </style>
