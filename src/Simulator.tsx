c4c154c914d68b3be93da94a0eb8d894c6a49de6
Alexandre TRICHOT <contact@alexandretrichot.fr>
2020-12-11 11:12:03 +0100

    Setup project

---
+++ src/Simulator.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {
+
+}
+
+export default class App extends React.Component<Props> {
+  state = {
+
+  }
+
+  render() {
+    return <div className="simulator">
+    </div>
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/scss/index.scss';
+import Simulator from './Simulator';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Simulator />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
