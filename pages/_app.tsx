c9936f8d3d8e8d419da6893fcf332055d998db6b
Alexandre TRICHOT <contact@alexandretrichot.fr>
2020-12-14 16:29:50 +0100

    Testing vercel storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from 'next/app';
 import '../assets/scss/index.scss';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
