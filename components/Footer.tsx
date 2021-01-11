209602d9e9832aea7bfaffd1ff00cc615dfa1e9a
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-01-11 15:01:50 +0100

    Update Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,13 @@
 export default function footer() {
   return <footer id="app-footer">
     <div className="sponsors">
+      <p>Ce site à été conçu sur l'initiative de <a href="https://meusenature.fr/">Meuse Nature Environnement</a> par <a href="https://github.com/alexandretrichot/">Alexandre TRICHOT</a></p>
+
       <p>Avec la participation de :</p>
       <div className="flex">
-        <img src={require('../assets/images/meuse.png')} alt="meuse logo" />
-        <img src={require('../assets/images/grand-est.jpg')} alt="grand est logo" />
-        <img src={require('../assets/images/dreal.jpg')} alt="dreal logo" />
+        <img src={require('../assets/images/meuse.png')} alt="Logo de la Meuse" />
+        <img src={require('../assets/images/grand-est.jpg')} alt="Logo du Grand-Est" />
+        <img src={require('../assets/images/dreal.jpg')} alt="Logo de la DREAL" />
       </div>
     </div>
   </footer>;
