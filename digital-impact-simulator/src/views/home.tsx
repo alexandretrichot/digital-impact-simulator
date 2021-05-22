715dc92264c570fa11ef13cc9e156d8953523776
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-05-22 12:58:56 +0200

    refactor: add homepage

--- digital-impact-simulator/src/views/home.tsx
+++ digital-impact-simulator/src/views/home.tsx
@@ -1,6 +1,31 @@
-function HomePage() {
+import './home.scss';
+
+import Lottie from '../components/lottie';
+import { Link } from 'react-router-dom';
+
+const HomePage: React.FC = () => {
 	return (
-		<h2>Home</h2>
+		<div id="home" className="page wrapper">
+			<div className="flex">
+				<Lottie animData={import('../assets/animations/booking.json')} />
+				<div>
+					<p>
+						Vous êtes-vous déjà demandé si votre consommation d’internet avait un
+						impact sur l’environnement ?
+        </p>
+					<p>
+						Le cas échéant, avez-vous une idée de ce que cela représente ? Ce que
+						cela consomme en électricité (en Wh) et en émissions de gaz à effets
+          de serre (équivalents de g de CO<sub>2</sub>) ?
+        </p>
+					<p>
+						Pour vous rendre compte de votre impact, nous vous proposons ce
+						simulateur d'impact.
+        </p>
+					<Link className="btn" to="/simulate">Faites le test !</Link>
+				</div>
+			</div>
+		</div>
 	);
 }
 
