0ef98b717b3e871753906a6954b8fad51fd59d66
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-05-31 21:10:01 +0200

    feat: update result to show versus

--- digital-impact-simulator/src/components/simulation/result.tsx
+++ digital-impact-simulator/src/components/simulation/result.tsx
@@ -15,6 +15,7 @@ import Simile from './simile';
 
 export type Props = {
 	stats: Stats,
+	vsStats?: Stats,
 }
 
 const Result: React.FC<Props> = props => {
@@ -23,11 +24,16 @@ const Result: React.FC<Props> = props => {
 	const ges = getGesStats(props.stats);
 	const totalGes = Object.values(ges).reduce((a, b) => a + b, 0);
 
+	const vsGwh = props.vsStats ? getKWhStats(props.vsStats) : [];
+	const totalVsKwh = Object.values(vsGwh).reduce((a, b) => a + b, 0);
+	const vsGes = props.vsStats ? getGesStats(props.vsStats) : [];
+	const totalVsGes = Object.values(vsGes).reduce((a, b) => a + b, 0);
+
 	return (
 		<section id="result">
 			<div className="wrapper">
 				<h2>Résumé de votre impact sur une journée</h2>
-				<Sum kwh={totalKwh} ges={totalGes} />
+				<Sum kwh={totalKwh} ges={totalGes} vsKwh={props.vsStats && totalVsKwh} vsGes={props.vsStats && totalVsGes} />
 
 				<p>
 					Répartition de votre consommation électrique liée à votre utilisation de
@@ -46,18 +52,13 @@ const Result: React.FC<Props> = props => {
 				<br />
 
 				<h3>En une année cela représente :</h3>
-				<Sum kwh={totalKwh * 365} ges={totalGes * 365} />
+				<Sum kwh={totalKwh * 365} ges={totalGes * 365} vsKwh={props.vsStats ? totalVsKwh * 265 : undefined} vsGes={props.vsStats ? totalVsGes * 265 : undefined} />
 				<br />
 				<p>À titre de comparaison c'est :</p>
 				<br />
 				<div className="similes">
-					<Simile title={`${Math.round((totalKwh * 365) / values.frigoPerYear)} frigos`} icon={fridgeIcon}>
-						C'est l'équivalent en consommation energétique de {Math.round((totalKwh * 365) / values.frigoPerYear)} frigos par an.
-        </Simile>
-					<Simile
-						title={`${((totalGes * 365) / values.gesPerKmInCar).toLocaleString()} Km`} icon={carIcon}>
-						C'est l'équivalent en émissions carbone d'une voiture thermique ayant parcouru {((totalGes * 365) / values.gesPerKmInCar).toLocaleString()} Km.
-        </Simile>
+					<Simile title={`${Math.round((totalKwh * 365) / values.frigoPerYear)} frigos`} icon={fridgeIcon}>C'est l'équivalent en consommation energétique de {Math.round((totalKwh * 365) / values.frigoPerYear)} frigos par an.</Simile>
+					<Simile title={`${((totalGes * 365) / values.gesPerKmInCar).toLocaleString()} Km`} icon={carIcon}>C'est l'équivalent en émissions carbone d'une voiture thermique ayant parcouru {((totalGes * 365) / values.gesPerKmInCar).toLocaleString()} Km.</Simile>
 				</div>
 			</div>
 		</section>
