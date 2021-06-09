06b73cc9b7f3226e665cfb6a980213d552f8870f
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-06-09 16:16:35 +0200

    feat: add script to send emails

---
+++ mailer/src/send.ts
@@ -0,0 +1,37 @@
+import nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { getText, getHtml } from './content';
+
+//const baseUrl = 'https://numerique.meusenature.fr/';
+const baseUrl = 'http://localhost:3000/';
+
+export const send = async (to: string, sessionId: string) => {
+	const transporter = nodemailer.createTransport({
+		host: process.env['EMAIL_HOST'],
+		port: Number(process.env['EMAIL_PORT']),
+		secure: true,
+		auth: {
+			user: process.env['EMAIL_USER'],
+			pass: process.env['EMAIL_PASSWORD'],
+		},
+	});
+
+	const link = `${baseUrl}simulate?compareTo=${sessionId}&self`;
+
+	const ret = await transporter.sendMail({
+		from: '"Meuse Nature Environnement" <noreply@meusenature.fr>',
+		to,
+		subject: `Testez votre impact du digital sur l'environnement ✔`,
+		text: getText(link),
+		html: getHtml(link),
+	});
+
+	return ret as {
+		accepted: string[],
+		rejected: string[],
+		envelopeTime: number,
+		messageTime: number,
+		messageSize: number,
+		response: string,
+	} & SMTPTransport.SentMessageInfo
+};
