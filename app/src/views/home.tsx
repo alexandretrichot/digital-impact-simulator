bddee0eb1acf57f6a0be3af4782a57e0a954b058
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-07-22 14:45:55 +0200

    feat: improve redaction

--- app/src/views/home.tsx
+++ app/src/views/home.tsx
@@ -8,24 +8,19 @@ const HomePage: React.FC = () => {
 		<div id="home" className="page wrapper">
 			<div className="intro">
 				<h3>Votre consommation d’internet a-t-elle un impact sur l’environnement ?</h3>
-				<p>Pour le découvrir, faites le test !</p>
+				<p>Vigilante sur la consommation exponentielle et généralisée d'objets connectés et d'internet, <a href="https://meusenature.fr/">Meuse Nature Environnement</a> s'est interrogée sur les impacts écologiques de cette "dématérialisation". Pour donner à chacun(e) une idée des impacts de ses principaux usages d'internet, nous avons conçu un simulateur en ligne qui estime la consommation électrique et les rejets de gaz à effets de serre induits par ses usages. Une bonne base pour commencer si l'on souhaite réduire son impact efficacement !</p>
 			</div>
-			<h2 className="centered">Je fais le test...</h2>
 			<div className="split">
 				<div className="card">
-					<div className="text">Seul</div>
+					<div className="text">Je fais le test seul</div>
 					<div className="anim"><Lottie anim="booking" /></div>
-					<Link className="btn" to="/simulate">Faire le test !</Link>
+					<Link className="btn" to="/simulate">C'est parti</Link>
 				</div>
 				<div className="card">
-					<div className="text">En groupe</div>
+					<div className="text">Je fais le test en groupe</div>
 					<div className="anim"><Lottie anim="friends" /></div>
 					<Link className="btn" to="/session">Créer un groupe</Link>
 				</div>
-			</div>
-			<div className="card">
-				<p>Depuis plusieurs années, <a href="https://meusenature.fr/">Meuse Nature Environnement</a> se questionne sur les impacts de la "dématérialisation" généralisée des activités et s'alarme sur la consommation exponentielle d'objets connectés et d'internet en général. Pour mieux appréhender l'impact que peut avoir notre usage généralisé d'internet, nous vous proposons ce simulateur.</p>
-				<p>A partir de votre expérience personnelle d'internet, il vous donnera une idée de l'impact de votre usage d'internet sur la consommation électrique et sur les rejets de gaz à effet de serre dans l'atmosphère, de façon globale et également par type d'utilisation. Un bon début pour déjà savoir où on en est et par où commencer pour réduire son impact efficacement !</p>
 			</div>
 		</div>
 	);
--- app/src/views/simulate.tsx
+++ app/src/views/simulate.tsx
@@ -38,9 +38,7 @@ const SimulatePage: React.FC = () => {
 								<p>Ce simulateur vous donnera une idée de l'impact global de votre usage d'internet sur la consommation électrique et sur les rejets de gaz à effet de serre dans l'atmosphère.</p>
 								<br />
 
-								{compare.data && (
-									<Info>La session actelle sera comparé avec <b>{self ? 'vos précédents résultats' : 'les résultats de votre ami'}</b>.</Info>
-								)}
+								{compare.data && <Info>La session actelle sera comparée avec <b>{self ? 'vos précédents résultats' : 'les résultats de votre ami'}</b>.</Info>}
 							</header>
 
 							<Simulator stats={session.stats} onStatsChange={stats => setSession({ stats, from: compareTo ? compareTo : undefined })} compareTo={compare.data ? { stats: compare.data.stats, self } : undefined} />
@@ -48,13 +46,13 @@ const SimulatePage: React.FC = () => {
 							<div className="wrapper">
 								<div>
 									<p>Comme vous pouvez le constater, internet n'est pas "immatériel" comme on l'entend souvent : nos activités "dématérialisées" ont un impact bien réel.</p>
-									<p>De façon invisible mais bien concrète, notre utilisation d'internet est à l'origine de consommations d'électricité et de rejets de gaz à effet de serre conséquents, contribuant à la surconsommation, au gaspillage de ressources, au dérèglement climatique et à des inégalités et injustices majeures...</p>
-									<p>Et la plupart de nos usages sont liés à des activités de loirirs et non des activités stratégiques, indispensables ou permettant a minima de contribuer à l'amélioration ou a préservation des conditions de vie sur Terre.</p>
+									<p>De façon invisible mais bien concrète, notre usage d'internet contribue à la surconsommation, au gaspillage de ressources, au dérèglement climatique et à des inégalités et injustices majeures...</p>
 									<br />
-									<p>Vous voulez en savoir plus ? Vous avez envie de réduire votre impact ? Nous vous proposons de consulter cette brochure de 16 pages sur les impacts environnementaux du numérique réalisée en 2021 par Meuse Nature Environnement.</p>
+									<p>Vous voulez en savoir plus ? Consultez <a href="https://meusenature.fr/publications/numerique" target="_blank" rel="noreferrer">notre brochure éditée en Août</a>.</p>
 								</div>
 
 								<EmailCTA scheduled={!!session.email} onSetEmail={email => setSession({ ...session, email, emailDate: new Date(new Date().getTime() + (30 * 24 * 60 * 60 * 1000)) })} />
+
 								<Share link={`${window.location.host}/simulate?compareTo=${session._id}`} />
 							</div>
 						</>
